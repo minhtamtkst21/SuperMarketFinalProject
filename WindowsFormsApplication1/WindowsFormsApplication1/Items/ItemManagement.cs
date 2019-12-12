c8aa86a9f0802eb110b3f0b8f01d5b1a81729474
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-12-12 16:41:53 +0700

    a

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Items/ItemManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Items
+{
+    class ItemManagement
+    {
+        private MyDataEntities db;
+
+    }
+}
