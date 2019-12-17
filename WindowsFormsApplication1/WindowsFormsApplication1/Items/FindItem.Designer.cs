06da99318d0545429558d59052ae82542d83ac19
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-12-17 14:51:26 +0700

    100

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Items/FindItem.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication1.Items
+{
+    partial class FindItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtcode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtproducer = new System.Windows.Forms.TextBox();
+            this.txtquantity = new System.Windows.Forms.TextBox();
+            this.txtprice = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantiy:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Price:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Code:";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(66, 13);
+            this.txtid.Name = "txtid";
+            this.txtid.ReadOnly = true;
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 6;
+            // 
+            // txtcode
+            // 
+            this.txtcode.Location = new System.Drawing.Point(66, 40);
+            this.txtcode.Name = "txtcode";
+            this.txtcode.ReadOnly = true;
+            this.txtcode.Size = new System.Drawing.Size(100, 20);
+            this.txtcode.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(66, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtproducer
+            // 
+            this.txtproducer.Location = new System.Drawing.Point(66, 93);
+            this.txtproducer.Name = "txtproducer";
+            this.txtproducer.ReadOnly = true;
+            this.txtproducer.Size = new System.Drawing.Size(100, 20);
+            this.txtproducer.TabIndex = 9;
+            // 
+            // txtquantity
+            // 
+            this.txtquantity.Location = new System.Drawing.Point(66, 120);
+            this.txtquantity.Name = "txtquantity";
+            this.txtquantity.ReadOnly = true;
+            this.txtquantity.Size = new System.Drawing.Size(100, 20);
+            this.txtquantity.TabIndex = 10;
+            // 
+            // txtprice
+            // 
+            this.txtprice.Location = new System.Drawing.Point(66, 147);
+            this.txtprice.Name = "txtprice";
+            this.txtprice.ReadOnly = true;
+            this.txtprice.Size = new System.Drawing.Size(100, 20);
+            this.txtprice.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FindItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 220);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtprice);
+            this.Controls.Add(this.txtquantity);
+            this.Controls.Add(this.txtproducer);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtcode);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FindItem";
+            this.Text = "FindItem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtcode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtproducer;
+        private System.Windows.Forms.TextBox txtquantity;
+        private System.Windows.Forms.TextBox txtprice;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Items/FindItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Items
+{
+    public partial class FindItem : Form
+    {
+        private ItemManagement business;
+        private int itemId;
+        public FindItem(int id)
+        {
+            InitializeComponent();
+            this.Load += FindItem_Load;
+            this.button1.Click += button1_Click;
+            itemId = id;
+            business = new ItemManagement();
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void FindItem_Load(object sender, EventArgs e)
+        {
+            var item=this.business.getItems(this.itemId);
+            this.txtid.Text = item.Id.ToString();
+            this.txtcode.Text = item.Code;
+            this.txtName.Text = item.Name;
+            this.txtprice.Text = item.Price.ToString();
+            this.txtproducer.Text = item.Producer.ToString();
+            this.txtquantity.Text = item.Amount.ToString();
+        }
+    }
+}
